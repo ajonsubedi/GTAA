8f9db1b112a2e4e27dcff420d75f278c8a4fb742
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2020-04-08 21:45:32 +0200

    Opties menu is klaar
    ik heb hier fullscreen,graphics en . volume instellingen toegevoegd.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour {
 
+	public AudioMixer audioMixer;
 	public void PlayGame () {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
@@ -14,6 +16,21 @@ public class MainMenu : MonoBehaviour {
 		Debug.Log("Quit!");
 		UnityEditor.EditorApplication.isPlaying = false;
 		Application.Quit();
+	}
+
+    public void SetVolume(float volume)
+	{
+		audioMixer.SetFloat("Volume", volume);
+	}
+
+	public void SetQuality(int qualityvalue)
+	{
+		QualitySettings.SetQualityLevel(qualityvalue);
+	}
+
+	public void SetFullScreen(bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
 	}
 
 }
